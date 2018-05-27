d379f88a7e325ef6f37477363aba182feba6fd7a
Basit.Sofi <basithamid@gmail.com>
2018-05-27 16:44:12 +0530

    server

--- rest.py
+++ rest.py
@@ -16,7 +16,6 @@ connection_params = {
 }
 
 
-
 connection = MongoClient(
     'mongodb://{user}:{password}@{host}:'
     '{port}/{namespace}'.format(**connection_params)
@@ -24,41 +23,50 @@ connection = MongoClient(
 
 db = dbClient.MongoDatabase(connection_params)
 
-@app.route('/getDishes', methods= ['GET'])
+
+@app.route('/getDishes', methods= ['GET', 'POST'])
 def getDishes():
     if request.method == 'POST':
         return codes.invalidRequestMethod()
     else:
         return db.getExistingDishes()
 
-@app.route('/getOrders', methods=['GET'])
+
+@app.route('/getOrders', methods=['GET', 'POST'])
 def getOrders():
     if request.method == 'POST':
         return codes.invalidRequestMethod()
     else:
         return db.getOrders()
 
-@app.route('/placeOrder', methods=['POST'])
+
+@app.route('/placeOrder', methods=['POST', 'GET'])
 def placeOrder():
     if request.method == 'GET':
         return codes.invalidRequestMethod()
     else:
         return db.placeOrder(request.get_json())
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'GET':
         return codes.invalidRequestMethod()
     else:
         return db.predictOrders(request.get_json())
 
-@app.route('/updateOrder', methods=['POST'])
+
+@app.route('/updateOrder', methods=['POST', 'GET'])
 def updateOrder():
     if request.method == 'GET':
         return codes.invalidRequestMethod()
     else:
         return db.update(request.get_json())
 
+
 @app.route('/')
 def index():
     return '<h1>Connected to server.</h1>'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000)
